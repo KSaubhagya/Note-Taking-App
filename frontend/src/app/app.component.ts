bcd67d4c1e3571ced6f4a57785c6007058e68cc6
KSaubhagya <saubhagyakrhmk.21@uom.lk>
2025-10-12 20:21:02 +0530

    Initial UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NoteListComponent } from './components/note-list/note-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [NoteListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
